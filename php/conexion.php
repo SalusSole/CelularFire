52732b1c65f463a17c4f31cd711893a3bdc41fe3
Salvador Solís Iñiguez <ch1.yomero.ss@gmail.com>
2020-05-21 18:29:36 -0500

    Conexion para el servdor actualizada

--- php/conexion.php
+++ php/conexion.php
@@ -1,3 +1,4 @@
 <?php
+    //$conexion=mysqli_connect('localhost', 'u850652173_salvador', 'celular_fire', 'u850652173_celularfire');
     $conexion=mysqli_connect('localhost', 'root', '', 'proyecto');
 ?>
