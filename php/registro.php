072abda00ccb0bd3c8ede052c3fd0cb3c125e8bb
Salvador Solís Iñiguez <ch1.yomero.ss@gmail.com>
2020-05-22 18:57:51 -0500

    Estructura HTML corregida

--- php/registro.php
+++ php/registro.php
@@ -64,16 +64,35 @@ if(!empty($_POST)){
                 $from = "admin@celularfireonline.com";
                 $to = $_POST['email'];
                 $subject = "Verifique su correo electronico";
-                $message = "<html>".
-                            "<body>".
-                            "<h1>Para realizar compras es necesario que verifique su cuenta de correo para la cuenta $user</h1>".
-                            "</body>".
-                            "</html>".
+                
+                $message = "
+                <html>
+                <head>
+                <title>HTML email</title>
+                </head>
+                <body>
+                <p>This email contains HTML Tags!</p>
+                <table>
+                <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                </tr>
+                <tr>
+                <td>John</td>
+                <td>Doe</td>
+                </tr>
+                </table>
+                </body>
+                </html>
+                ";
 
-                // $message = 'Para poder realizar compras es necesario que verifique su cuenta de correo. Si este es su correo haga click en el siguiente enlace para ser redirigido a Celular Fire. <br> <center> <a class="btn" href="http://localhost/cf-four/php/logout.php?estado_cuenta=1">Empieza a comprar ahora.</a></center>';
-                $headers = 'MIME-Version: 1.0' . "\r\n";
-                $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-                $headers = "From:" . $from;
+                // Always set content-type when sending HTML email
+                $headers = "MIME-Version: 1.0" . "\r\n";
+                $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+                // More headers
+                $headers .= 'From:' . $from. "\r\n";
+
                 mail($to,$subject,$message, $headers);
                 echo "The email message was sent.";
 
