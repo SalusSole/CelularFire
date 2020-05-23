d96e4904ab8c4bcfeb3a1ccc14acbabccc0be7f2
Salvador Solís Iñiguez <ch1.yomero.ss@gmail.com>
2020-05-22 21:40:51 -0500

    Modificación en el modulo de verificacion.

--- php/verificacion.php
+++ php/verificacion.php
@@ -1,9 +1,12 @@
 <?php
 
-if($_GET){
+if ($_GET) {
     $user = $_GET['nombre_usuario'];
     include 'conexion.php';
-    $sql = "UPDATE `user` SET `estado_cuenta` = '1' WHERE `user`.`fullname` = $user"; 
+    // $update="Update user Set estado_cuenta='1' Where fullname='$user'"
+    $sql = "UPDATE user SET estado_cuenta='1' WHERE username='$user'";
+    $conexion->query($sql);
+        print "<script>window.location='../index.php';</script>";
+    
 }
-
 ?>
