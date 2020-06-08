ef4f2c99b7748600d4d3411974daca7c5f4851a9
Salvador Solís Iñiguez <ch1.yomero.ss@gmail.com>
2020-06-08 09:57:59 -0500

    Se agregó paginador a productos admin

--- productos_admin.php
+++ productos_admin.php
@@ -2,6 +2,7 @@
     session_start();
 
     include 'php/conexion.php';
+    include 'php/variables_paginador.php';
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -70,6 +71,51 @@
     </div>
     </div>
     </div>
+    <div class="paginador">
+<!-- Paginador con bootstrap  -->
+<?php if($row>=10): ?>
+    <div class="">
+        <div aria-label="Page navigation example">
+        <ul class="pagination">
+
+            <?php
+            if ($_GET['page']>1){
+            ?>
+                <li class="page-item">
+                <a class="page-link" href="index.php?page=<?php echo $_GET['page']-1 ?>" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                </li>
+            <?php
+            }
+            ?>
+
+            <?php for($i=0;$i<$pages;$i++):?>
+                <li class='page-item <?php echo $_GET['page']==$i+1 ? 'active' : '' ?>'>
+                    <a class='page-link' href='index.php?page=<?php echo $i+1 ?>'>
+                        <?php echo $i+1 ?>
+                    </a>
+                </li>
+            <?php endfor ?>
+            
+            <?php
+            if ($_GET['page']<$pages){
+            ?>
+                <li class="page-item">
+                <a class="page-link" href="index.php?page=<?php echo $_GET['page']+1 ?>" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span>
+                </a>
+                </li>
+            <?php
+            }
+            ?>
+        </ul>
+        </div>
+    </div>
+    <?php endif ?>
+</div>
 </body>
 </html>
 <?php
