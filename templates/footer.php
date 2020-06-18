477afd16709dcf9742866b5fe24eb2946fc9bb60
Salvador Solís Iñiguez <ch1.yomero.ss@gmail.com>
2020-06-18 13:00:47 -0500

    Actualización en footer.

--- templates/footer.php
+++ templates/footer.php
@@ -2,31 +2,31 @@
 <div class="line">
 <footer>
     <div class="container">
-        <div class="contenedor">
+        <div class="contenedor text-center">
             <div class="row">
-                <div class="col-4">
-                    <strong class="text-center">Atención al cliete</strong><br>
-                    <span>CelularFire@gmail.com</span><br>
-                    <span>Contacto</span>
+                <div class="col-md-4 col-sm-6">
+                    <strong class="text-center">Celular Fire</strong><br>
                 </div>
-                <div class="col-4">
-                    <strong class="text-center">Información legal</strong>
-                </div>
-                <div class="col-4">
-                    <strong class="text-center">Siguenos en</strong>
+                <div class="col-md-4 col-sm-6">
+                    <strong class="text-center">Inicia ahora</strong>
                     <br>
-                    <a href="https://www.facebook.com/CelularFire/"><img class="img-fluid" width="50em" src="img/iconos/icons/facebook.png" alt="Facebook"></a>
-                    <a href="https://instagram.com/celu430?igshid=17gj2z8xgxqhm"><img class="img-fluid" width="50px" src="img/iconos/icons/instagram.png" alt="Instagram"></a>
+                    <br>
+                    <a href="registro.php" class="btn btn-info">Registrate</a>
+                </div>
+                
+                <div class="col-md-4 col-sm-12">
+                    <strong class="text-center">Contactanos</strong>
+                    <br>
+                    <a href="https://www.facebook.com/CelularFire/"><img class="img-fluid" width="40em" src="img/iconos/icons/facebook.png" alt="Facebook"></a>
+                    <a href="https://instagram.com/celu430?igshid=17gj2z8xgxqhm"><img class="img-fluid" width="40em" src="img/iconos/icons/instagram.png" alt="Instagram"></a>
+                    <br>
+                    <a>info@celularfireonline.com</a>
                 </div>  
             </div>
             <div class="row">
-                <div class="col-4">
+                <div class="col-12">
                     <br>
-                    <strong class="text-center">Su compra esta segura</strong>
-                </div>
-                <div class="col-8">
-                    <br>
-                    <a href="ayuda.php" class="footer-ayuda"><strong class="text-center">Ayuda</strong></a>
+                    <a href="ayuda.php" class="footer-ayuda"><strong>Ayuda</strong></a>
                 </div>
             </div>
         </div>
