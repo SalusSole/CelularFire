77008b0cd46f2dc03af648c0567ef5365353ee77
Salvador Solís Iñiguez <ch1.yomero.ss@gmail.com>
2020-06-24 18:13:25 -0500

    Conexion a DB actualizada Se agrego la conexion a la nueva base de datos.

--- php/conexion.php
+++ php/conexion.php
@@ -1,4 +1,4 @@
 <?php
-    //$conexion=mysqli_connect('localhost', 'u850652173_salvador', 'celular_fire', 'u850652173_celularfire');
+    //$conexion=mysqli_connect('localhost', '	u850652173_admin', 'adminCF2020', 'u850652173_celular_fire');
     $conexion=mysqli_connect('localhost', 'root', '', 'proyecto');
 ?>
